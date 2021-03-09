659261cb8f72cf6c97e7f603bb1343793c036a43
Bobby Pascua <r.pascua@berkeley.edu>
2021-03-09 01:03:55 -0800

    update: make better plots in visualization script

--- pset3/fdtd/scripts/make_frames.py
+++ pset3/fdtd/scripts/make_frames.py
@@ -7,19 +7,23 @@ if __name__ == "__main__":
     args = sys.argv
     data_file = args[1]
     save_dir = args[2]
-    try:
-        field = args[3]
-    except IndexError:
-        field = "Hzx"
 
-    data = np.load(data_file)[field]
-    vmin = data.min()
-    vmax = data.max()
+    data = np.load(data_file, allow_pickle=True)
+    Ex = data["Ex"]
+    Ey = data["Ey"]
+    Hz = data["Hzx"] + data["Hzy"]
+    labels = ("Ex", "Ey", "Hz")
     basename = os.path.splitext(os.path.split(data_file)[1])[0]
-    for i in range(data.shape[0]):
-        fig, ax = plt.subplots(figsize=(5,5), dpi=150)
-        cax = ax.imshow(data[i].T, aspect='auto', vmin=vmin, vmax=vmax)
-        fig.colorbar(cax)
+    for i in range(1, data["Ntimes"]):
+        fig, axes = plt.subplots(1, 3, figsize=(15,5), dpi=150)
+        for ax, field in zip(axes, (Ex, Ey, Hz)):
+            ax.imshow(
+                np.abs(field[i]).T / np.abs(field[i]).max(),
+                aspect='auto',
+                origin="lower",
+                vmin=0,
+                vmax=1
+            )
         filename = os.path.join(save_dir, f"{basename}_frame_{i}.png")
         fig.savefig(filename, bbox_inches='tight', dpi=150)
         plt.close(fig)
