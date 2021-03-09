db4d8435405da2924ca0d995675104c83aa6c16b
Bobby Pascua <r.pascua@berkeley.edu>
2021-03-09 04:09:49 -0800

    update: normalize plots to H field

--- pset3/fdtd/scripts/make_frames.py
+++ pset3/fdtd/scripts/make_frames.py
@@ -2,6 +2,7 @@ import os
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+from astropy import constants
 
 if __name__ == "__main__":
     args = sys.argv
@@ -10,15 +11,26 @@ if __name__ == "__main__":
 
     data = np.load(data_file, allow_pickle=True)
     Ex = data["Ex"]
+    Ex = 0.5 * (Ex[:,:,1:] + Ex[:,:,:-1])
     Ey = data["Ey"]
+    Ey = 0.5 * (Ey[:,1:,:] + Ey[:,:-1,:])
     Hz = data["Hzx"] + data["Hzy"]
+    permeability = data["relative_permeability"]
+    impedance = np.sqrt(constants.mu0.value / constants.eps0.value)
     labels = ("Ex", "Ey", "Hz")
     basename = os.path.splitext(os.path.split(data_file)[1])[0]
     for i in range(1, data["Ntimes"]):
+        if (100 * i / data["Ntimes"]) % 10 == 0:
+            print(f"{100 * i / data['Ntimes']}% done.")
         fig, axes = plt.subplots(1, 3, figsize=(15,5), dpi=150)
-        for ax, field in zip(axes, (Ex, Ey, Hz)):
+        for ax, field, name in zip(axes, (Ex, Ey, Hz), ("Ex", "Ey", "Hz")):
+            ax.set_title(name)
+            if name == "Hz":
+                scale = np.abs(field[i]).max()
+            else:
+                scale = impedance * permeability * np.abs(Hz[i]).max()
             ax.imshow(
-                np.abs(field[i]).T / np.abs(field[i]).max(),
+                np.abs(field[i]).T / scale,
                 aspect='auto',
                 origin="lower",
                 vmin=0,
